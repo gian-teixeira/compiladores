3f5057cacdd8e472626f0e7de9e6a3ad2e4e4ff9
Gian <giancarlo.oliveira.teixeira@gmail.com>
2024-10-21 06:48:27 -0300

    new way

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,133 +1,158 @@
 use crate::token::Token;
 use std::collections::HashMap;
 
-pub enum State {
-    Base,
+enum State {
+    Base(Option<fn(&String) -> Option<Token>>),
     Double(char, Token, Token),
     Name,
     Integer,
     Float
 }
 
-pub fn parse(src : &String)
--> Vec<Token>
-{
-    let keyword_table = Token::get_keyword_table();
+pub struct Parser {
+    keyword_table : HashMap<String, Token>
+}
 
-    let mut state = State::Base;
-    let mut tokens : Vec<Token> = Vec::new();
-    let mut buffer : String = String::new();
-    let mut to_buffer : bool;
+impl Parser {
+    pub fn to_int(buffer : &String)
+    -> Option<Token>
+    {
+        buffer.parse::<i64>().map_or(None, |v| {
+            let wrapper = Token::IntConst(v);
+            Some(wrapper)
+        })
+    }
+    
+    pub fn to_float(buffer : &String)
+    -> Option<Token>
+    {
+        buffer.parse::<f32>().map_or(None, |v| {
+            let wrapper = Token::FloatConst(v);
+            Some(wrapper)
+        })
+    }
 
-    for c in src.chars() {
-        println!("{buffer}");
+    pub fn to_single_operator(buffer : &String)
+    -> Option<Token>
+    {
+        match &buffer[..] {
+            ")" => Some(Token::LBracket),
+            "(" => Some(Token::RBracket),
+            "}" => Some(Token::LBrace),
+            "{" => Some(Token::RBrace),
+            "]" => Some(Token::LCol),
+            "[" => Some(Token::RCol),
+            "+" => Some(Token::Plus),
+            "-" => Some(Token::Minus),
+            "*" => Some(Token::Mult),
+            "/" => Some(Token::Div),
+            ";" => Some(Token::PComma),
+            "," => Some(Token::Comma),
+            _ => None
+        }
+    }
 
-        to_buffer = true;
+    pub fn to_double_operator(buffer : &String)
+    -> Option<Token>
+    {
+        match &buffer[..] {
+            "==" => Some(Token::EQ),
+            "!=" => Some(Token::NEQ),
+            ">=" => Some(Token::GE),
+            "<=" => Some(Token::LE),
+            _ => None
+        }
+    }
 
-        match state {
-            
-            // Base point and single component operators
-            State::Base => {
-                if c.is_alphabetic() {
-                    state = State::Name;
-                }
-                else if c.is_numeric() {
-                    state = State::Integer;
-                }
-                else {
-                    to_buffer = false;
+    pub fn to_key(buffer : &String)
+    -> Option<Token>
+    {
+        let token = match &buffer[..] {
+            "int" => Token::Int,
+            _ => Token::Id(buffer.clone())
+        };
+        Some(token)
+    }
+    
+    pub fn parse(src : &String)
+    -> Vec<Token>
+    {
+        let mut state = State::Base(None);
+        let mut tokens : Vec<Token> = Vec::new();
+        let mut buffer : String = String::new();
+        let mut to_buffer : bool;
 
-                    let single_token = match c {
-                        ')' => Some(Token::LBracket),
-                        '(' => Some(Token::RBracket),
-                        '}' => Some(Token::LBrace),
-                        '{' => Some(Token::RBrace),
-                        ']' => Some(Token::LCol),
-                        '[' => Some(Token::RCol),
-                        '+' => Some(Token::Plus),
-                        '-' => Some(Token::Minus),
-                        '*' => Some(Token::Mult),
-                        '/' => Some(Token::Div),
-                        ';' => Some(Token::PComma),
-                        ',' => Some(Token::Comma),
-                        _ => None
-                    };
+        for c in src.chars() {
+            println!("{buffer}");
 
-                    if single_token.is_some() {
-                        tokens.push(single_token.unwrap());
-                        state = State::Base;
+            to_buffer = true;
+
+            match state {
+                // Base point and single component operators
+                State::Base(buffer_parser) => {
+                    if buffer_parser.is_some() {
+                        let token = buffer_parser.unwrap()(&buffer);
+                        tokens.push(token.unwrap());
+                        buffer = String::new();
+                    }
+
+                    if c.is_alphabetic() {
+                        state = State::Name;
+                    }
+                    else if c.is_numeric() {
+                        state = State::Integer;
                     }
                     else {
-                        let double_state = match c {
-                            '=' => Some(State::Double('=', Token::EQ, Token::Attr)),
-                            '!' => Some(State::Double('=', Token::NEQ, Token::Not)),
-                            '>' => Some(State::Double('=', Token::GE, Token::GT)),
-                            '<' => Some(State::Double('=', Token::LE, Token::LT)),
-                            _ => None
-                        };
-                        if double_state.is_some() {
-                            state = double_state.unwrap();
+                        match String::from("(){}[]+-*/;,").find(c) {
+                            Some(_) => state = State::Base(Some(Parser::to_single_operator)),
+                            None => match String::from("=!><").find(c) {
+                                Some(_) => state = State::Base(Some(Parser::to_double_operator)),
+                                None => {}
+                            }
                         }
                     }
                 }
-            }
 
-            // Double component operators
-            State::Double(ref expected, ref result, ref except) => {
-                tokens.push(
-                    if c == *expected { result.clone() }
-                    else { except.clone() }
-                );
-                state = State::Base;
-                to_buffer = false;
-            }
-
-            // Names
-            State::Name => {
-                if !c.is_alphanumeric() && c != '_' {
-                    let default_token = Token::Id(buffer.clone());
-                    let token = keyword_table
-                        .get(&buffer)
-                        .unwrap_or(&default_token);
-                    tokens.push(token.clone());
-                    buffer = String::new();
-                    state = State::Base;
+                // Double component operators
+                State::Double(ref expected, ref result, ref except) => {
+                    tokens.push(
+                        if c == *expected { result.clone() }
+                        else { except.clone() }
+                    );
+                    state = State::Base(None);
+                    to_buffer = false;
                 }
-            }
 
-            // Integer
-            State::Integer => {
-                if c == '.' {
-                    state = State::Float;
-                }
-                else if !c.is_numeric() {
-                    match buffer.parse::<i64>() {
-                        Ok(int_value) => tokens.push(Token::IntConst(int_value)),
-                        Err(e) => println!("{e}")
+                // Names
+                State::Name => {
+                    if !c.is_alphanumeric() && c != '_' {
+                        state = State::Base(Some(Parser::to_key));
                     }
-                    buffer = String::new();
-                    state = State::Base;
                 }
-            }
 
-            // Float
-            State::Float => {
-                if !c.is_numeric() {
-                    match buffer.parse::<f32>() {
-                        Ok(float_value) => tokens.push(Token::FloatConst(float_value)),
-                        Err(e) => println!("{e}")
+                // Integer
+                State::Integer => {
+                    if c == '.' {
+                        state = State::Float;
                     }
-                    buffer = String::new();
-                    state = State::Base;
+                    else if !c.is_numeric() {
+                        state = State::Base(Some(Parser::to_int));
+                    }
                 }
+
+                // Float
+                State::Float => {
+                    if !c.is_numeric() {
+                        state = State::Base(Some(Parser::to_float));
+                    }
+                }
+            }            
+            
+            if to_buffer && !c.is_control() && !c.is_whitespace() {
+                buffer.push(c);
             }
-        }            
-        
-        if to_buffer && !c.is_control() && !c.is_whitespace() {
-            buffer.push(c);
         }
+        
+        return tokens;
     }
-    
-    return tokens;
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod lexer;
 mod token;
 
+use lexer::Parser;
+
 pub fn main() {
     let file_content = std::fs::read_to_string("tmp.txt")
         .expect("Should have been able to read the file");
-    let tokens = lexer::parse(&file_content);
+    let tokens = Parser::parse(&file_content);
     for token in tokens {
         print!("{token:?} ");
     }
